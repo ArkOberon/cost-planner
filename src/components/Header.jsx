0a23c241978b81b36225039ec07a93f666ce5829
xenon <pmcruz@hermenautas.com>
2022-10-09 19:24:58 +0200

    new frontend style

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import NewBudget from "./NewBudget"
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Cost Planner APP</h1>
+      <NewBudget />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/NewBudget.jsx
@@ -0,0 +1,19 @@
+const NewBudget = () => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form className="formulario">
+        <div className="campo">
+          <label>Your Budget</label>
+          <input 
+            className="nuevo-presupuesto"
+            type="text"
+            placeholder="Add your budget"          
+          />
+        </div>
+        <input type="submit" value="Add" />
+      </form>
+    </div>
+  )
+}
+
+export default NewBudget
