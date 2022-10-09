f7254a1fc0997dfc073781813bf05da54641b414
xenon <pmcruz@hermenautas.com>
2022-10-09 21:26:38 +0200

    validator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import NewBudget from "./NewBudget"
 
-const Header = () => {
+const Header = ( { budget, setBudget } ) => {
   return (
     <header>
       <h1>Cost Planner APP</h1>
-      <NewBudget />
+      <NewBudget 
+        budget={budget}
+        setBudget={setBudget}     
+      />
     </header>
   )
 }
--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -1,16 +1,38 @@
-const NewBudget = () => {
+import { useState } from "react"
+import Message from "./Message"
+
+const NewBudget = ({ budget, setBudget }) => {
+
+  const [message, setMessage] = useState("")
+
+  const handleBudget = (e) => {
+    e.preventDefault()
+
+    if(!Number(budget) || Number(budget) < 0) {
+      setMessage("No es un presupuesto valido")
+    } else {
+      console.log("Es valido")
+    }
+
+  }
+
   return (
     <div className="contenedor-presupuesto contenedor sombra">
-      <form className="formulario">
+      <form onSubmit={handleBudget} className="formulario">
         <div className="campo">
           <label>Your Budget</label>
           <input 
             className="nuevo-presupuesto"
             type="text"
-            placeholder="Add your budget"          
+            placeholder="Add your budget" 
+            value={budget}
+            onChange={ (e) => setBudget(e.target.value) }         
           />
         </div>
         <input type="submit" value="Add" />
+
+        {message && <Message tipo="error">{message}</Message>}
+
       </form>
     </div>
   )
